6240e0ee6a366928a290ee44320c0348db266e74
Peter Müller <munter@fumle.dk>
2015-07-10 13:28:10 +0200

    Also test devDependencies

--- index.js
+++ index.js
@@ -6,6 +6,43 @@ var chalk = require('chalk');
 var express = require('express');
 var app = express();
 
+function testModule(moduleName, done) {
+
+  var dom = jsdom.env({
+    url: 'http://localhost:9001/' + moduleName + '.html',
+    features: {
+      FetchExternalResources: ['script', 'img', 'css', 'frame', 'iframe', 'link'],
+      ProcessExternalResources: ['script']
+    },
+    scripts: [
+      jspm.directories.packages + '/system.js'
+    ],
+    done: function (errs, window) {
+      if (errs) {
+        console.error(window.location.href, errs);
+      }
+
+      setTimeout(function () {
+        var System = window.System;
+
+        System.import(jspm.configFile.replace('.js', ''))
+          .then(function () {
+            return System.import(moduleName)
+          })
+          .then(function (module) {
+            console.log(chalk.green('LOADED'), moduleName);
+          })
+          .catch(function (err) {
+            console.error(chalk.red('FAILED'), moduleName, err);
+          })
+          .then(done);
+      }, 500);
+
+    }
+  });
+
+}
+
 app
   .get('*.html', function (req, res) {
     res.end('');
@@ -14,40 +51,12 @@ app
 
 app.listen(9001, function () {
 
-  async.each(Object.keys(jspm.dependencies), function (moduleName, done) {
+  console.log('\nDependencies');
+  async.each(Object.keys(jspm.dependencies), testModule, function () {
 
-    var dom = jsdom.env({
-      url: 'http://localhost:9001/' + moduleName + '.html',
-      features: {
-        FetchExternalResources: ['script', 'img', 'css', 'frame', 'iframe', 'link'],
-        ProcessExternalResources: ['script']
-      },
-      scripts: [
-        jspm.directories.packages + '/system.js'
-      ],
-      done: function (errs, window) {
-        if (errs) {
-          console.error(window.location.href, errs);
-        }
+    console.log('\nDevelopment Dependencies');
+    async.each(Object.keys(jspm.devDependencies), testModule, process.exit);
 
-        setTimeout(function () {
-          var System = window.System;
-
-          System.import(jspm.configFile.replace('.js', ''))
-            .then(function () {
-              return System.import(moduleName)
-            })
-            .then(function (module) {
-              console.log(chalk.green('LOADED'), moduleName);
-            })
-            .catch(function (err) {
-              console.error(chalk.red('FAILED'), moduleName, err);
-            })
-            .then(done);
-        }, 500);
-
-      }
-    });
-  }, process.exit);
+  });
 
 });
