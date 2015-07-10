5b99f8ebdb2ab11d8831cf2b8180a83a597440c4
Peter Müller <munter@fumle.dk>
2015-07-10 17:58:57 +0200

    Switch to promises and also test local path setup

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
 var jspm = require(process.cwd() + '/package.json').jspm;
 var jsdom = require('jsdom');
-var async = require('async');
 var chalk = require('chalk');
+var When = require('when');
+var System = require('systemjs');
 
 var express = require('express');
 var app = express();
 
-function testModule(moduleName, done) {
+function testModule(moduleName) {
+  return When.promise(function (resolve, reject) {
 
-  var dom = jsdom.env({
-    url: 'http://localhost:9001/' + moduleName + '.html',
-    features: {
-      FetchExternalResources: ['script', 'img', 'css', 'frame', 'iframe', 'link'],
-      ProcessExternalResources: ['script']
-    },
-    scripts: [
-      jspm.directories.packages + '/system.js'
-    ],
-    done: function (errs, window) {
-      if (errs) {
-        console.error(window.location.href, errs);
+    var dom = jsdom.env({
+      url: 'http://localhost:9001/' + moduleName + '.html',
+      features: {
+        FetchExternalResources: ['script', 'img', 'css', 'frame', 'iframe', 'link'],
+        ProcessExternalResources: ['script']
+      },
+      scripts: [
+        jspm.directories.packages + '/system.js'
+      ],
+      done: function (errs, window) {
+        if (errs) {
+          console.error(window.location.href, errs);
+        }
+
+        setTimeout(function () {
+          var System = window.System;
+
+          System.import(jspm.configFile.replace('.js', ''))
+            .then(function () {
+              return System.import(moduleName)
+                .then(function () { return moduleName; });
+            })
+            .then(resolve)
+            .catch(reject)
+        }, 500);
       }
+    });
 
-      setTimeout(function () {
-        var System = window.System;
+  });
+}
 
-        System.import(jspm.configFile.replace('.js', ''))
-          .then(function () {
-            return System.import(moduleName)
-          })
-          .then(function (module) {
-            console.log(chalk.green('LOADED'), moduleName);
-          })
-          .catch(function (err) {
-            console.error(chalk.red('FAILED'), moduleName, err);
-          })
-          .then(done);
-      }, 500);
-
+function logResults(results) {
+  results.forEach(function (result) {
+    if (result.state === 'fulfilled') {
+      console.log(chalk.green('LOADED'), result.value);
+    } else {
+      console.log(chalk.red('FAILED'), result.reason);
     }
   });
-
 }
 
 app
@@ -51,12 +59,42 @@ app
 
 app.listen(9001, function () {
 
-  console.log('\nDependencies');
-  async.each(Object.keys(jspm.dependencies), testModule, function () {
+  When.settle(Object.keys(jspm.dependencies).map(testModule))
+    .tap(function (results) {
+      console.log('\nDependencies');
+    })
+    .tap(logResults)
 
-    console.log('\nDevelopment Dependencies');
-    async.each(Object.keys(jspm.devDependencies), testModule, process.exit);
+    //////////
 
-  });
+    .then(function () {
+      return When.settle(Object.keys(jspm.devDependencies).map(testModule));
+    })
+    .tap(function (results) {
+      console.log('\nDevelopment Dependencies');
+    })
+    .tap(logResults)
+
+    //////////
+
+    .then(function () {
+      return System.import(jspm.configFile).then(function () {
+        var mappings = System.map;
+
+        var localPaths = Object.keys(mappings).filter(function (moduleName) {
+          return moduleName.indexOf(':') === -1 && mappings[moduleName].indexOf(':') === -1;
+        });
+
+        return When.settle(localPaths.map(testModule));
+      });
+    })
+    .tap(function (results) {
+      console.log('\nLocal mappings');
+    })
+    .tap(logResults)
+
+    //////////
+
+    .finally(process.exit);
 
 });
